b9c50530641af1a09e55cc12023d0281c86d3c2c
ppfeiffer <admin@pfeiffer-privat.de>
2025-05-08 06:33:39 +0200

    erste Anpassungen

---
+++ build-tnnpi.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# get pi-gen sources
+git clone https://github.com/RPi-Distro/pi-gen
+cd pi-gen
+git fetch && git fetch --tags
+#git checkout 2020-02-05-raspbian-buster # Up to image v1.0.8-beta10
+#git checkout 2020-12-02-raspbian-buster # Up to image v1.1
+git checkout 2021-03-04-raspbian-buster # Used for image v1.3
+#git checkout 2023-02-21-raspios-bullseye # Used for images after Dec 2023 v1.4
+#git checkout 2023-12-05-raspios-bookworm # v1.5.2 - caused issues on Raspi Zero and 5 (rolled back)
+#git 2024-03-12-raspios-bullseye # v1.5.3
+cd ..
+
+# copy config
+[ -e pi-gen/config ] && rm -R pi-gen/config
+cp config pi-gen/config
+
+# copy custom stage
+[ -e pi-gen/stage-tnn ] && rm -R pi-gen/stage-tnn
+cp -R stage-tnn pi-gen/stage-tnn
+
+case "$OSTYPE" in
+  darwin*)
+	echo "Preparing sed to work with OSX"
+	sed -i -e 's/sed -r/sed -E/g' pi-gen/build-docker.sh
+	;;
+esac
+
+echo "Running build..."
+cd pi-gen
+#PRESERVE_CONTAINER=0 CONTINUE=0 ./build-docker.sh
+CLEAN=1 ./build.sh
+
