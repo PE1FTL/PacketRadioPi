15d622610a7a8cc655696864fc8ea6d07a45c72b
PE1FTL <admin@pfeiffer-privat.de>
2025-05-19 19:37:46 +0200

    Update build-tnnpi.sh

--- build-tnnpi.sh
+++ build-tnnpi.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# install dependencies
+sudo apt update
+sudo apt install -y curl quilt parted realpath qemu-user-static debootstrap zerofree pxz zip dosfstools bsdtar libcap2-bin grep rsync xz-utils git
+
 # get pi-gen sources
 git clone https://github.com/RPi-Distro/pi-gen
 cd pi-gen
@@ -10,6 +14,7 @@ git checkout 2021-03-04-raspbian-buster # Used for image v1.3
 #git checkout 2023-02-21-raspios-bullseye # Used for images after Dec 2023 v1.4
 #git checkout 2023-12-05-raspios-bookworm # v1.5.2 - caused issues on Raspi Zero and 5 (rolled back)
 #git 2024-03-12-raspios-bullseye # v1.5.3
+rm -rf ./stage[345]
 cd ..
 
 # copy config
@@ -21,8 +26,7 @@ cp config pi-gen/config
 cp -R stage-tnnpi pi-gen/stage-tnnpi
 
 # only minimalsystem
-touch pi-gen/stage3/SKIP pi-gen/stage4/SKIP pi-gen/stage5/SKIP
-touch pi-gen/stage2/SKIP_NOOBS pi-gen/stage2/SKIP_IMAGES pi-gen/stage4/SKIP_IMAGES pi-gen/stage5/SKIP_IMAGES
+touch pi-gen/stage2/SKIP_NOOBS pi-gen/stage2/SKIP_IMAGES 
 #rm pi-gen/stage2/EXPORT_IMAGE pi-gen/stage2/EXPORT_NOOBS
 
 case "$OSTYPE" in
