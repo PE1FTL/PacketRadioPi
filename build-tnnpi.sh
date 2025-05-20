c1a7b0db43fde5d9ceca7981969593e910ce07ad
PE1FTL <admin@pfeiffer-privat.de>
2025-05-20 14:17:12 +0200

    Update build-tnnpi.sh
    change buster version

--- build-tnnpi.sh
+++ build-tnnpi.sh
@@ -10,7 +10,7 @@ cd pi-gen
 git fetch && git fetch --tags
 #git checkout 2020-02-05-raspbian-buster # Up to image v1.0.8-beta10
 #git checkout 2020-12-02-raspbian-buster # Up to image v1.1
-git checkout 2021-03-04-raspbian-buster # Used for image v1.3
+git checkout 2023-05-03-raspios-buster # Used for image v1.3
 #git checkout 2023-02-21-raspios-bullseye # Used for images after Dec 2023 v1.4
 #git checkout 2023-12-05-raspios-bookworm # v1.5.2 - caused issues on Raspi Zero and 5 (rolled back)
 #git 2024-03-12-raspios-bullseye # v1.5.3
