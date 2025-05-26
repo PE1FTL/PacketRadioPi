e1e4eba8490737d7c87204728ed569ee05e1e15e
ppfeiffer <admin@pfeiffer-privat.de>
2025-05-26 19:55:16 +0200

    fehlerkorrektur

--- stage-tnnpi/03-ax25/00-run.sh
+++ stage-tnnpi/03-ax25/00-run.sh
@@ -2,7 +2,7 @@
 
 # configure firstboot options
 install -m 775 files/InstAX25.sh "${ROOTFS_DIR}/usr/local/ax25"
-install -m 775 files/firstboot.sh "${ROOTFS_DIR}/root/firstboot.sh"
+
 
 on_chroot << EOF
 	raspi-config nonint do_memory_split 16
