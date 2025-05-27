a3463f35d87e8c9b477f014173547bf618db02e4
ppfeiffer <admin@pfeiffer-privat.de>
2025-05-27 18:25:43 +0200

    neuer Versuch

--- stage-tnnpi/03-ax25/00-run.sh
+++ stage-tnnpi/03-ax25/00-run.sh
@@ -7,9 +7,13 @@ run_in_chroot() {
 $1
 EOF
 }
+#  make build-dir
+echo "mk build dir"
+run_in_chroot "
+mkdir -p ${ROOTFS_DIR}/usr/local/ax25/
+"
 
 # configure firstboot options
-mkdir -p "${ROOTFS_DIR}/usr/local/ax25/"
 install -m 775 files/InstAX25.sh "${ROOTFS_DIR}/usr/local/ax25/InstAX25.sh"
 
 
