31b9f66d36755d820c009d226b2599ae4e1e4849
ppfeiffer <admin@pfeiffer-privat.de>
2025-06-01 15:44:51 +0200

    AX25 Pakete eingefügt

--- stage-tnnpi/03-ax25/00-run.sh
+++ stage-tnnpi/03-ax25/00-run.sh
@@ -9,15 +9,15 @@ EOF
 }
 
 # configure options
-install -d "${ROOTFS_DIR}/usr/local/ax25"
-install -m 775 files/InstAX25.sh "${ROOTFS_DIR}/usr/local/ax25/InstAX25.sh"
+#install -d "${ROOTFS_DIR}/usr/local/ax25"
+#install -m 775 files/InstAX25.sh "${ROOTFS_DIR}/usr/local/ax25/InstAX25.sh"
 
 
 # compile tnn-ax25
-echo "compile tnn-ax25"
-run_in_chroot "
-cd /usr/local/ax25
-./InstAX25.sh
-ln -s /usr/local/etc/ax25 /etc/ax25
-ln -s /usr/local/var/ax25 /var/ax25
-"
+#echo "compile tnn-ax25"
+#run_in_chroot "
+#cd /usr/local/ax25
+#./InstAX25.sh
+#ln -s /usr/local/etc/ax25 /etc/ax25
+#ln -s /usr/local/var/ax25 /var/ax25
+#"
